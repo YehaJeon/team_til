56a21e92b957cb224c9904ce01e73a299f71a5b6
YeajinRyu0101 <edinryu0101@gmail.com>
2022-02-11 19:59:27 +0900

    220211

--- yeajin/challenges/2nd/index.js
+++ yeajin/challenges/2nd/index.js
@@ -1,19 +1,18 @@
 const back=document.querySelector("body");
 const colors=["ivory","violet","rgb(96, 138, 228)","rgb(190, 245, 168)","rgb(240, 140, 140)"];
 
-back.style.backgroundColor=colors[0];
-
 function resize(){
     const whole=window.innerWidth;
-    if(whole<=200){
+    if(whole<=700){
         back.style.backgroundColor=colors[1];
-    }else if(whole>200 && whole <=400 ){
+    }else if(whole>700 && whole <=900 ){
         back.style.backgroundColor = colors[2]
-    }else if(whole>400 && whole <=600){
+    }else if(whole>900 && whole <=1100){
         back.style.backgroundColor = colors[3]
-    }else if(whole>800 &&whole <=1000){
+    }else if(whole>1100 &&whole <=1300){
         back.style.backgroundColor=colors[4]
+    }else{
+        back.style.backgroundColor=colors[0]
     }
 }
-
 window.addEventListener("resize",resize);
