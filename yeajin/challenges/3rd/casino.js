e8bea9ffa119baa622ff5c02e760ce4927a34be4
YeajinRyu0101 <edinryu0101@gmail.com>
2022-02-16 00:21:15 +0900

    220215

--- yeajin/challenges/3rd/casino.js
+++ yeajin/challenges/3rd/casino.js
@@ -1,2 +1,28 @@
-const number=[1,2,3,4,5,6,7,8,9,10];
-const number=number[Math.floor(Math.random()*quotes.length)];
+const range=document.querySelector("#rangeNum");
+const guess=document.querySelector("#choseNum");
+const playButton=document.querySelector("#btn");
+const alarm=document.querySelector("#notice");
+const result=document.querySelector("#result");
+function gameStart(event){
+  event.preventDefault();
+
+  const rangeNum=range.value;
+  const guessNum=guess.value;
+  const randomNum= Math.ceil(Math.random()*rangeNum);
+  alarm.classList.remove("hidden");
+  alarm.innerText=`You chose: ${guessNum}, the machine chose: ${randomNum}`
+  if(Number(randomNum)==(guessNum)){
+    result.classList.remove("hidden");
+    result.innerText="You Won!";
+  }
+  else if(Number(rangeNum)<Number(guessNum)){
+    result.classList.remove("hidden");
+    result.innerText="Please choose number in range"
+
+  }
+  else{
+    result.classList.remove("hidden");
+    result.innerText="You lost~";
+  }
+}
+playButton.addEventListener("click", gameStart);
