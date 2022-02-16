5860339dd3aef38a3400f4dbfa4825d4f4638ba9
YeajinRyu0101 <edinryu0101@gmail.com>
2022-02-17 01:38:02 +0900

    220216

--- yeajin/challenges/4th/timer.js
+++ yeajin/challenges/4th/timer.js
@@ -1,6 +1,16 @@
-const dday=new Date();
-const today=new Date(2022,11,25);
-const timer=today-dday;
-console.log(dday);
-console.log(today);
-console.log(timer);
+const clockTitle=document.querySelector(".js-clock");
+
+
+setInterval(
+function hello(){
+const today=new Date();
+const dDay=new Date(2022,11,25);
+const gap=dDay.getTime()-today.getTime();
+const second = Math.floor((gap / 1000) % 60);
+const minute = Math.floor((gap / 1000 / 60) % 60);
+const hour = Math.floor(gap / 1000 / 60 / 60) % 24;
+const day = Math.floor(gap / 1000 / 60 / 60 / 24);
+  clockTitle.innerText= `${day}d ${hour < 10 ? `0${hour}` : hour}h ${minute < 10 ? `0${minute}` : minute}m ${
+    second < 10 ? `0${second}` : second}s`;
+},1000);
+
