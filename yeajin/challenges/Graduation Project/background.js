9424264e636799f9365c10e868f25d4a6cc8da7f
Yeajin Ryu <96979210+YeajinRyu0101@users.noreply.github.com>
2022-02-23 01:52:17 +0900

    220223

--- yeajin/challenges/Graduation Project/background.js
+++ yeajin/challenges/Graduation Project/background.js
@@ -2,6 +2,6 @@ const images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9
 const chosenImage=images[Math.floor(Math.random()*images.length)];
 console.log(chosenImage);
 const bgImage= document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 console.log(bgImage);
 document.body.appendChild(bgImage);
