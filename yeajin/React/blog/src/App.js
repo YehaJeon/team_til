a17699634c4a2172f273a9642520db1428613c33
Yeajin Ryu <96979210+YeajinRyu0101@users.noreply.github.com>
2022-03-10 05:44:55 +0900

    220309

--- yeajin/React/blog/src/App.js
+++ yeajin/React/blog/src/App.js
@@ -8,12 +8,18 @@ function App() {
   let[따봉,따봉변경]=useState(0);
   let posts ='딸기 라떼 맛집';
   
+  function 제목바꾸기(){
+    var newArray=[...글제목];
+    newArray[0]='여자 코트 추천'
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div style={{color:'blue', fontSize :'30px'}}>개발 blog</div>
       </div>
-      <button>버튼</button>
+      
         <div className='list'>
           <h3>{글제목[0]}<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span>{따봉}</h3>
           <p>3월 7일 발행</p>
@@ -29,7 +35,21 @@ function App() {
           <p>3월 9일 발행</p>
           <hr/>
         </div>
+<button onClick={제목바꾸기}>버튼</button>
+        <Modal />
+
     </div>
   );
 }
+
+function Modal(){
+  return(
+    <div className='modal'>
+    <h2>제목</h2>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
+}
+
 export default App;
