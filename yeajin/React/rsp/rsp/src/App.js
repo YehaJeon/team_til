9050e073cbfb13b8d498e6ee2606fb2e3243b2ad
Yeajin Ryu <96979210+YeajinRyu0101@users.noreply.github.com>
2022-03-12 03:56:53 +0900

    220311

--- yeajin/React/rsp/rsp/src/App.js
+++ yeajin/React/rsp/rsp/src/App.js
@@ -23,4 +23,40 @@ function App() {
   );
 }
 
+
+
+const reducer = (state, action) => {
+  const data = state.history;
+
+  switch (action.type) {
+    case 'ADD':
+      return {
+        ...state,
+        history: !data ? [action.newGame] : [...data, action.newGame]
+      };
+    case 'RESULT':
+      let _comWin, _youWin;
+      console.log(action.whoWins);
+      console.log(state.comWin);
+      if (action.whoWins === 'computer') {
+        _comWin = state.comWin ? parseInt(state.comWin) + 1 : 1;
+        _youWin = state.youWin;
+      }
+      if (action.whoWins === 'you') {
+        _comWin = state.comWin;
+        _youWin = state.youWin ? parseInt(state.youWin) + 1 : 1;
+      }
+      if (action.whoWins === 'draw') {
+        _comWin = state.comWin;
+        _youWin = state.youWin;
+      }
+      return {
+        ...state,
+        comWin: _comWin,
+        youWin: _youWin
+      };
+    case 'RESET':
+      return {};
+  }
+};
 export default App;
