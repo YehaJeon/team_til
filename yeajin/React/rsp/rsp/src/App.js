0163a7918841b1ed26ab4d9198f6fa55334559ba
Yeajin Ryu <96979210+YeajinRyu0101@users.noreply.github.com>
2022-03-14 23:26:36 +0900

    220314

--- yeajin/React/rsp/rsp/src/App.js
+++ yeajin/React/rsp/rsp/src/App.js
@@ -1,62 +1,19 @@
-import React,{useState} from 'react';
+import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const rsp = {
+  바위 :
+  가위 :
+  보:
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      
     </div>
   );
 }
-
-
-
-const reducer = (state, action) => {
-  const data = state.history;
-
-  switch (action.type) {
-    case 'ADD':
-      return {
-        ...state,
-        history: !data ? [action.newGame] : [...data, action.newGame]
-      };
-    case 'RESULT':
-      let _comWin, _youWin;
-      console.log(action.whoWins);
-      console.log(state.comWin);
-      if (action.whoWins === 'computer') {
-        _comWin = state.comWin ? parseInt(state.comWin) + 1 : 1;
-        _youWin = state.youWin;
-      }
-      if (action.whoWins === 'you') {
-        _comWin = state.comWin;
-        _youWin = state.youWin ? parseInt(state.youWin) + 1 : 1;
-      }
-      if (action.whoWins === 'draw') {
-        _comWin = state.comWin;
-        _youWin = state.youWin;
-      }
-      return {
-        ...state,
-        comWin: _comWin,
-        youWin: _youWin
-      };
-    case 'RESET':
-      return {};
-  }
-};
+;
 export default App;
